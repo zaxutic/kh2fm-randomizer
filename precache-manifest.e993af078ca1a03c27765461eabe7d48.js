cbf23fea9bca6401ce78937a964cfb0bd9089f5f
afresquet <alvarofresquet@gmail.com>
2020-05-11 00:50:51 +0200

    Updates

---
+++ precache-manifest.e993af078ca1a03c27765461eabe7d48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddcbcefa2066d8898b3a2754bd643251",
+    "url": "/kh2fm-randomizer/index.html"
+  },
+  {
+    "revision": "baa9e1807c9adc12a6c1",
+    "url": "/kh2fm-randomizer/static/js/2.7b5029b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kh2fm-randomizer/static/js/2.7b5029b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15af1b96f64c632f2232",
+    "url": "/kh2fm-randomizer/static/js/main.47bbf0ec.chunk.js"
+  },
+  {
+    "revision": "28736f1388f9622fe0dd",
+    "url": "/kh2fm-randomizer/static/js/runtime-main.674eae6b.js"
+  }
+]);
