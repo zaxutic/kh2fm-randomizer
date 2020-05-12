080be748806fdc8fd46eb350476d6c75125d8209
afresquet <alvarofresquet@gmail.com>
2020-05-12 05:50:18 +0200

    Updates

---
+++ precache-manifest.e41ba6e238d7d9601e2e10fdb59e26c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d9740fc785188b28818f1b5a08b57e",
+    "url": "/kh2fm-randomizer/index.html"
+  },
+  {
+    "revision": "4f2b41eee4fa48ae649a",
+    "url": "/kh2fm-randomizer/static/js/2.2a2d88bd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kh2fm-randomizer/static/js/2.2a2d88bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9f6a2bb3453ce3287f5",
+    "url": "/kh2fm-randomizer/static/js/main.469a0ca2.chunk.js"
+  },
+  {
+    "revision": "28736f1388f9622fe0dd",
+    "url": "/kh2fm-randomizer/static/js/runtime-main.674eae6b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kh2fm-randomizer/precache-manifest.866da5aa64c1055e203097003f38eeac.js"
+  "/kh2fm-randomizer/precache-manifest.e41ba6e238d7d9601e2e10fdb59e26c5.js"
 );
 
 self.addEventListener('message', (event) => {
