497e0d72ef178fe4cfdf791b7651c904dd3ef9b7
afresquet <alvarofresquet@gmail.com>
2020-06-23 16:35:27 +0200

    Updates

---
+++ precache-manifest.f1403d77aa5a88a9ecd789970454532d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce610c812097ddbd756abc854b789e7",
+    "url": "/kh2fm-randomizer/index.html"
+  },
+  {
+    "revision": "0ee5c95d4007c0b6131b",
+    "url": "/kh2fm-randomizer/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "11aaef75628f6d3d76e6",
+    "url": "/kh2fm-randomizer/static/css/main.61effe11.chunk.css"
+  },
+  {
+    "revision": "0ee5c95d4007c0b6131b",
+    "url": "/kh2fm-randomizer/static/js/2.0f803a35.chunk.js"
+  },
+  {
+    "revision": "121176ba3fca79ab1bfc07fa7daca3df",
+    "url": "/kh2fm-randomizer/static/js/2.0f803a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11aaef75628f6d3d76e6",
+    "url": "/kh2fm-randomizer/static/js/main.f60f5882.chunk.js"
+  },
+  {
+    "revision": "28736f1388f9622fe0dd",
+    "url": "/kh2fm-randomizer/static/js/runtime-main.674eae6b.js"
+  }
+]);
